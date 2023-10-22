50590ed743117c0af326438f6859e7078d335d80
Shudhanshuarya09 <104650751+sid7258054054@users.noreply.github.com>
2023-10-22 16:59:51 +0530

    inverted pyramid

--- invertedfullpyramid.cpp
+++ invertedfullpyramid.cpp
@@ -6,13 +6,13 @@ int main()
     cin>>n;
     for(int row=0; row<n; row++)
     {
-        for(int col=0; col<n-1-row; col++)
-        {
-            cout <<" ";
-        }
         for(int col=0; col<row+1; col++)
         {
-            cout <<"* ";
+            cout<< " ";
+        }
+        for(int col=0; col<n-row; col++)
+        {
+            cout<< "* ";
         }
         cout<<endl;
     }
