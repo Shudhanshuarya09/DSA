3deafec0d0f3524a1295a01a6aa7144a48b304c9
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2023-10-23 12:16:45 +0530

    pyramid

---
+++ numericfullpyramid.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int row=0; row<n; row++)
+    {
+        for(int col=0; col<n-row; col++)
+        {
+            cout <<"* ";
+        }
+        cout<<1;
+        for(int col=0; col<row+2; row++)
+        {
+            cout<<"row+col+1";
+
+        }
+        cout<<endl;
+    }
+}
