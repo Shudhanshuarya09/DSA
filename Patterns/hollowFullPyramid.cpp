a21f42e3c25529e331fa1447b0eac19cca364e8e
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-05-13 20:15:10 +0530

    Hollow inverted pyramid

--- Patterns/hollowFullPyramid.cpp
+++ Patterns/hollowFullPyramid.cpp
@@ -13,23 +13,23 @@ int main()
     int k = 0;
     for (int j = 0; j < ((2 * n) - 1); j++)
     {
-      if (j < n - i - 1)               //for spaces in the bignning
+      if (j < n - i - 1)
       {
         cout << " ";
       }
-      else if (k < 2 * i + 1)          //for stars in the middle (also conditioned apply for the hollow )
+      else if (k < 2 * i + 1)
       {
-        if (k == 0 || k == 2 *i || i == n - 1)
+        if (k == 0 || k == 2 * i || i == n - 1)
 
           cout << "*";
-
+        // cout<<" ";
         else
 
           cout << " ";
 
         k++;
       }
-      else                           //for the spaces in the end 
+      else
       {
         cout << " ";
       }
