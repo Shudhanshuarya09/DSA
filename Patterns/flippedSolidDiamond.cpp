c00a3b6c50da44de667852f8cf9381625a530605
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-05-17 13:02:54 +0530

    complete code of flipped sol Dimo

--- Patterns/flippedSolidDiamond.cpp
+++ Patterns/flippedSolidDiamond.cpp
@@ -3,24 +3,85 @@ using namespace std;
 
 int main()
 {
-  //PART-1
-  for (int row = 0; row < 1; row++)
+  // //PART-1
+  // for (int row = 0; row < 1; row++)
+  // {
+  //   for (int col = 0; col < 10; col++)
+  //   {
+  //     cout << " * ";
+  //   }
+  //   cout << endl;
+  // }
+
+  // //PART-2
+  //   for (int row = 1; row < 5; row++)
+  // {
+  //   for (int col = 0; col < 5 - row; col++)
+  //   {
+  //     cout << " * ";
+  //   }
+  //   cout << endl;
+  // }
+
+  // //PART-3
+  //     for (int row = 1; row < 5; row++)
+  // {
+  //   for (int col = 4; col < row*2; col++)
+  //   {
+  //     cout << " # ";
+  //   }
+  //   cout << endl;
+  // }
+
+  //  //PART-4
+  //     for (int row = 1; row < 5; row++)
+  // {
+  //   for (int col = 6; col < 5 - row; col++)
+  //   {
+  //     cout << " * ";
+  //   }
+  //   cout << endl;
+  // }
+
+  int n;
+  cin >> n;
+  for (int row = 0; row < n; row++)
   {
-    for (int col = 0; col < 10; col++)
+    // first part
+    for (int col = 0; col < n - row; col++)
     {
-      cout << " * ";
+      cout << "*";
+    }
+    for (int col = 0; col < 2*row +1 ; col++)
+    {
+      cout << " ";
+    }
+    for (int col = 0; col < n - row; col++)
+    {
+      cout << "*";
     }
     cout << endl;
   }
 
-  //PART-2
-    for (int row = 1; row < 5; row++)
-  {
-    for (int col = 0; col < 5 - row; col++)
+    // sedcond part
+
+    for (int row = 0; row < n; row++)
     {
-      cout << " * ";
+      for (int col = 0; col < row + 1; col++)
+      {
+        cout << "*";
+      }
+      for (int col = 0; col < 2*n - 2*row - 1; col++)
+      {
+        cout << " ";
+      }
+      for (int col = 0; col < row + 1; col++)
+      {
+        cout << "*";
+      }
+      cout << endl;
     }
-    cout << endl;
-  }
+    
+  
   
 }
