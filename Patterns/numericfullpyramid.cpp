9819af789272b664ba87855e63d2b4fb9cf80a4a
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-05-18 17:00:08 +0530

    Numeric Full Pyramid

--- Patterns/numericfullpyramid.cpp
+++ Patterns/numericfullpyramid.cpp
@@ -4,21 +4,27 @@ int main()
 {
     int n;
     cin>>n;
-    for(int row=0; row<n; row++)
-    {
-        for(int col=0; col<n-row; col++)
-        {
-            cout <<"* ";
-        }
-        for(int col=0; col<n-row; col++)
-        {
-        cout<<1+1;
-        }
-        // for(int col=0; col<row+2; row++)
-        // {
-        //     cout<<"* ";
 
-        // }
+    for (int row =0; row<n; row++)
+    {
+        //spaces
+        for (int col=0; col<n-row-1; col++)
+        {
+            cout<<" ";
+        }
+        //numbers
+        for(int col=0; col<row+1; col++)
+        {
+            cout<<row+col+1;
+        }
+        //reverse counting
+        int start = 2*row;
+        for(int col =0; col<row; col++)
+        {
+            cout<<start;
+            start=start-1;
+        }
         cout<<endl;
     }
+    
 }
