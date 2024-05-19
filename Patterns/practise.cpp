4b3b4a3a80a9e006fdc6fe44a02bd598e7e6d5d0
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-05-19 10:53:17 +0530

    GFG practice questions

--- Patterns/practise.cpp
+++ Patterns/practise.cpp
@@ -6,17 +6,30 @@ int main()
   int n;
   cin >> n;
 
-  for (int row = 0; row < n*2 - 1; row++)
-  {
-    int col =0;
-      if ( row == 0 || n*2 - 2 || col == 0 || n*2 -2)
-      {
-        for(; col<n*2 -1; col++)
-        {
-          cout<<n;
-        }
-      }
+  // for (int row = 0; row < n*2 - 1; row++)
+  // {
+  //   int col =0;
+  //     if ( row == 0 || n*2 - 2 || col == 0 || n*2 -2)
+  //     {
+  //       for(; col<n*2 -1; col++)
+  //       {
+  //         cout<<n;
+  //       }
+  //     }
     
-    cout << endl;
-  }
+  //   cout << endl;
+  // }
+
+   for (int row =0; row <n; row++)
+        {
+            for (int col =0; col<n-row-1; col++)
+            {
+                cout<<" ";
+            }
+            for (int col=0; col<2*row +1; col++)
+            {
+                cout<<"*";
+            }
+            cout<<endl;
+        }
 }
