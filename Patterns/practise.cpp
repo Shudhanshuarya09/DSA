a59ce88caff197658f3f6e8c129f3aa5cb15b4c2
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-05-20 20:24:07 +0530

    GFG question

--- Patterns/practise.cpp
+++ Patterns/practise.cpp
@@ -33,11 +33,40 @@ int main()
   //           cout<<endl;
   //       }
 
-   for (int row=0; row<n; row++)
+//    for (int row=0; row<n; row++)
+//         {
+//             for (int col=0; col<row+1; col++)
+//             {
+//                 cout<<col+1<<" ";
+//             }
+//             cout<<endl;
+//         }
+
+
+for (int row =0; row< n; row++)
         {
-            for (int col=0; col<row+1; col++)
+            for(int col =0; col<n-row-1; col++)
             {
-                cout<<col+1<<" ";
+                cout<<" ";
+            }
+            
+            for(int col=0; col<row+1;  col++)
+            {
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+        
+         for (int row =0; row< n; row++)
+        {
+            for(int col =0; col<row+1; col++)
+            {
+                cout<<" ";
+            }
+            
+            for(int col=0; col<n-row;  col++)
+            {
+                cout<<"*";
             }
             cout<<endl;
         }
