e65c037d54b22259e813c64fa587d0634cd4df7e
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-05-25 22:16:45 +0530

    post

--- prePost.cpp
+++ prePost.cpp
@@ -3,6 +3,11 @@ using namespace std;
 
 int main()
 {
-  int a = 5;
-  cout<<(++a)*(++a)<<endl;
+  // int a = 5;
+  // cout<<(++a)*(++a)<<endl;
+
+  //post increment
+
+  int a = 6;
+  cout<< (--a)<<endl;
 }
