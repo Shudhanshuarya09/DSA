ead569a424a1cd2cd756dad223bc363ada84e805
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-05-26 15:04:38 +0530

    Switch case

---
+++ switch.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n ;
+  cout<<"Enter the value"<<endl;
+  cin>>n;
+
+  switch(n)
+  {
+    case 1: cout<<"Shudhasnhu"<<endl;
+    break;
+    case 2: cout<<"Arya"<<endl;
+    break;
+     case 3: cout<<"Himanshu"<<endl;
+    break;
+     case 4: cout<<"Aryannn"<<endl;
+    break;
+    default : cout<<"Narendra Modi"<<endl;
+  }
+}
