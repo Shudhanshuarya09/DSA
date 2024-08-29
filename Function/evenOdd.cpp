4e65e0dd4f0dd705c500d5c8b7d964dcb546df32
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-08-29 22:11:46 +0530

    even or add problem

--- Function/evenOdd.cpp
+++ Function/evenOdd.cpp
@@ -1,29 +1,28 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
-
 
 int evenOdd(int n)
 {
-  if(n % 2 == 0)
+  if (n % 2 == 0)
   {
-    cout <<"This is even Number" <<endl;
+    cout << "This is even Number" << endl;
   }
   else
   {
-    cout<<"This is Odd Number" <<endl;
+    cout << "This is Odd Number" << endl;
   }
 
-  cout <<endl;
+  cout << endl;
 }
 
 int main()
 {
-int n;
-cout << "Enter the value of n : " << endl;
-cin >> n;
+  int n;
+  cout << "Enter the value of n : " << endl;
+  cin >> n;
 
-int findNumber = evenOdd(n);
-cout << findNumber <<endl;
+  int findNumber = evenOdd(n);
+  cout << findNumber << endl;
 
-return 0;
+  return 0;
 }
