2a5a99af9136cd6e455bae17c2d9bf8f73708a1e
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-08-29 22:05:22 +0530

    even odd

---
+++ Function/evenOdd.cpp
@@ -0,0 +1,29 @@
+#include <iostream> 
+using namespace std;
+
+
+int evenOdd(int n)
+{
+  if(n % 2 == 0)
+  {
+    cout <<"This is even Number" <<endl;
+  }
+  else
+  {
+    cout<<"This is Odd Number" <<endl;
+  }
+
+  cout <<endl;
+}
+
+int main()
+{
+int n;
+cout << "Enter the value of n : " << endl;
+cin >> n;
+
+int findNumber = evenOdd(n);
+cout << findNumber <<endl;
+
+return 0;
+}
