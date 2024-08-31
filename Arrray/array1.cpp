edfe4188c1de6695ed73b72629939c9b58dd5f0b
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-08-31 21:59:21 +0530

    Array

---
+++ Arrray/array1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  //let create array of size 5 and printing value at particular index
+  int aryaaaaa[] = {2,8,4,5,9,6};
+  // cout << aryaaaaa[5]  << endl;           //here 5 is index number and want to know the value at that index
+
+
+
+//for printing all numbers
+for(int i=0; i<=5; i++)
+{
+  cout << aryaaaaa[i] << " ";
+}
+
+return 0;
+}
---
+++ Arrray/array2.cpp
@@ -0,0 +1,23 @@
+//code for the taking input and print the output of Array
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int arr; 
+
+//taking input in array 
+for(int i=0; i<10; i++)
+{
+  cin >> arr[i] ;
+}
+
+//printing
+cout << "printing the values in array " << endl;
+for(int i=0; i<10; i++)
+{  
+  cout << arr[i] << " ";
+}
+
+return 0;
+}
