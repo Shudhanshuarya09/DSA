2c532c17601f04b88233f75fcbd43db270c45876
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-09-01 09:16:22 +0530

    arrray 2

--- Arrray/array2.cpp
+++ Arrray/array2.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 int main()
 {
-  int arr; 
-
+ 
+int arr[10];
 //taking input in array 
 for(int i=0; i<10; i++)
 {
   cin >> arr[i] ;
 }
-
+cout << "givve numbers" << endl;
 //printing
 cout << "printing the values in array " << endl;
 for(int i=0; i<10; i++)
