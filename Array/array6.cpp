594f87d84a8fd5ab7d6010e6248e6953f7bc2382
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-09-04 23:04:14 +0530

    array 6

--- Array/array6.cpp
+++ Array/array6.cpp
@@ -4,7 +4,18 @@ using namespace std;
 int main()
 {
 
+  int arr[] = {2, 4, 5, 1, 3, 7, 9, 57, 86, 91};
+  int size = 11;
 
-  ushefhisafghl
+  int maxi = INT_MIN;
+
+  for (int i = 0; i < size; i++)
+  {
+    if (arr[i] > maxi)
+    {
+      maxi = arr[i];
+    }
+  }
+  cout << "maximum number is " << maxi << endl;
   return 0;
 }
