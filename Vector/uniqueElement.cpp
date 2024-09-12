3b18181edb401ef388e38fbbd7be32aca8543d72
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-09-13 00:10:51 +0530

    unique number complete

--- Vector/uniqueElement.cpp
+++ Vector/uniqueElement.cpp
@@ -2,8 +2,33 @@
 #include<vector>
 using namespace std;
 
+find(vector<int>arr)
+{
+  int ans = 0;
+for(int i=0; i<arr.size(); i++)
+{
+  ans = ans ^ arr[i];
+}
+return ans;
+}
+
+
 int main()
 {
+int n;
+cout << "Enter the number" << endl;
+cin>>n;
+
+
+vector<int> arr(n);
+cout<<"Enter the elements in array" <<endl;
+for(int i=0; i<arr.size(); i++)
+{
+  cin>>arr[i];
+}
+
+int uniqueElement = find(arr);
+cout << "Unique number is :" << uniqueElement <<endl;
 
 
   
